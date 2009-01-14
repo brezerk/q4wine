3786cbcfb6a0fa7d395fd0998f69c15c9ed62ddb
Malakhov Alexey (John Brezerk) <brezerk@gmail.com>
2009-01-14 13:49:57 +0200

    Adding cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# This is cealnup script for cleaning make, tmp and other build files.
+#
+# Plz. Use it before commit project to git
+
+echo "Run: make clean";
+make clean;
+echo "Removing cmake tmp";
+rm -rdf CMakeCache.txt CMakeFiles config.h cmake_install.cmake Makefile build;
+echo "Removing *~ fiels";
+find ./ -name '*~' -delete;
+echo "Done";
