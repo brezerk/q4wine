b7536e717f722ad5cc609a8449c531d270488366
pasha <pasha@pashazz.(none)>
2009-03-05 16:05:49 +0000

    Exp Help

---
+++ src/temporary/winetrickshelp.cpp
@@ -0,0 +1,50 @@
+#include "winetrickshelp.h"
+#include "ui_winetrickshelp.h"
+
+winetrickshelp::winetrickshelp(QWidget *parent) :
+    QDialog(parent),
+    m_ui(new Ui::winetrickshelp)
+{
+    m_ui->setupUi(this);
+    QString WINETRICKS_FILE = QDir::homePath() + ".config/q4wine/winetricks";
+if (!QFile::exists(WINETRICKS_FILE)) {
+    QMessageBox msg;
+    msg.setText(tr("Please install winetricks: winetricks not found"));
+    msg.setIcon(QMessageBox::Critical);
+    msg.setWindowTitle(tr("winetricks not found"));
+    msg.show();
+this->close();
+return;
+}
+QProcess *p = new QProcess (this);
+p->start(QDir::homePath() + tr(".config/q4wine/winetricks -"));
+p->waitForFinished();
+QString lang;
+                // Getting env LANG variable
+                lang = getenv("LANG");
+                lang = lang.split(".").at(1);
+
+                if (lang.isNull())
+                        lang = "UTF8";
+QTextCodec *codec = QTextCodec::codecForName(lang.toAscii());
+QString string = codec->toUnicode(p->readAllStandardOutput());
+m_ui->txtOutput->setText(string);
+}
+
+winetrickshelp::~winetrickshelp()
+{
+    delete m_ui;
+}
+
+void winetrickshelp::changeEvent(QEvent *e)
+{
+    switch (e->type()) {
+    case QEvent::LanguageChange:
+        m_ui->retranslateUi(this);
+        break;
+    default:
+        break;
+    }
+}
+
+
---
+++ src/temporary/winetrickshelp.h
@@ -0,0 +1,32 @@
+#ifndef WINETRICKSHELP_H
+#define WINETRICKSHELP_H
+
+#include <QtGui/QDialog>
+#include <QProcess>
+#include <QFile>
+#include <QDir>
+#include <QMessageBox>
+#include <QTextCodec>
+
+namespace Ui {
+    class winetrickshelp;
+}
+
+class winetrickshelp : public QDialog {
+    Q_OBJECT
+    Q_DISABLE_COPY(winetrickshelp)
+public:
+    explicit winetrickshelp(QWidget *parent = 0);
+    virtual ~winetrickshelp();
+
+protected:
+    virtual void changeEvent(QEvent *e);
+
+private:
+    Ui::winetrickshelp *m_ui;
+
+private slots:
+    void on_buttonBox_clicked(QAbstractButton* button);
+};
+
+#endif // WINETRICKSHELP_H
