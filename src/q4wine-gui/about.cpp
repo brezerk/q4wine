0c9dc9310b47c9127474e86d7443664c9d2a3912
Malakhov Alexey <brezerk@gmail.com>
2010-08-19 09:14:01 +0300

    updates;

--- src/q4wine-gui/about.cpp
+++ src/q4wine-gui/about.cpp
@@ -150,6 +150,7 @@ About::About(QWidget * parent, Qt::WFlags f) : QDialog(parent, f)
 								   "<span style=\" color:#6495ed;\">E-Mail</span>: <span style=\" text-decoration: underline; color:#5f9ea0;\">brezerk@gmail</span>" \
 							"</p><p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
 								   "Kiev, Ukraine" \
+
 							"</p><p style=\"margin: 0px\"> </p>" \
 
                                                         "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
