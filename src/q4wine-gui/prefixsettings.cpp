6e456cdeaf3138dc5b59fe966d4a5b5d004730c2
Alex Richardson <alex.richardson@gmx.de>
2010-11-21 18:28:01 +0100

    Add combo box to set mountpoint_windrive and then store it in db

--- src/q4wine-gui/prefixsettings.cpp
+++ src/q4wine-gui/prefixsettings.cpp
@@ -193,13 +193,12 @@ void PrefixSettings::cmdOk_Click(){
     }
 
     if (this->addNew){
-        // FIXME: insert the correct value
-        if (!db_prefix.addPrefix(txtPrefixName->text(),  txtPrefixPath->text(), txtWineBin->text(), txtWineServerBin->text(), txtWineLoaderBin->text(), txtWineLibs->text(), txtMountPoint->text(), comboDeviceList->currentText(), comboArchList->currentText(), ""))
+        if (!db_prefix.addPrefix(txtPrefixName->text(),  txtPrefixPath->text(), txtWineBin->text(), txtWineServerBin->text(), txtWineLoaderBin->text(), txtWineLibs->text(), txtMountPoint->text(), comboDeviceList->currentText(), comboArchList->currentText(), this->comboWinDrive->currentText()))
             reject();
 
         CoreLib->createPrefixDBStructure(txtPrefixName->text());
     } else {
-        if (!db_prefix.updatePrefix(txtPrefixName->text(), txtPrefixPath->text(), txtWineBin->text(), txtWineServerBin->text(), txtWineLoaderBin->text(), txtWineLibs->text(), txtMountPoint->text(), comboDeviceList->currentText(), this->prefix_name, comboArchList->currentText(), ""))
+        if (!db_prefix.updatePrefix(txtPrefixName->text(), txtPrefixPath->text(), txtWineBin->text(), txtWineServerBin->text(), txtWineLoaderBin->text(), txtWineLibs->text(), txtMountPoint->text(), comboDeviceList->currentText(), this->prefix_name, comboArchList->currentText(), this->comboWinDrive->currentText()))
             reject();
     }
 
