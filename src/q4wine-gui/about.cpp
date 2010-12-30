f2589f674f2a26700ffccff6c86671964c56783f
Malakhov Alexey <brezerk@gmail.com>
2010-12-30 09:24:52 +0200

    Updated Yaron Shahrabani's information;

--- src/q4wine-gui/about.cpp
+++ src/q4wine-gui/about.cpp
@@ -210,7 +210,13 @@ About::About(QWidget * parent, Qt::WFlags f) : QDialog(parent, f)
                             "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
                                     "Yaron Shahrabani" \
                             "</p><p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
-                                    "Hebrew translator" \
+                                    "Hebrew translation" \
+                            "</p><p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
+                                    "<span style=\" color:#6495ed;\">E-Mail</span>: <span style=\" text-decoration: underline; color:#5f9ea0;\">sh.yaron@gmail.com</span>" \
+                            "</p><p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
+                                     "<span style=\" color:#6495ed;\">Web</span>: <span style=\" text-decoration: underline; color:#5f9ea0;\">https://launchpad.net/~sh-yaron</span>" \
+                            "</p><p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
+                                    "Misgav, Israel" \
                             "</p><p style=\"margin: 0px\"> </p>" \
 
                             "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
