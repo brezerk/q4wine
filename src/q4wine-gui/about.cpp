db57c79832ca739d265b1c22cab86cd2e0bd7785
Alexey S. Malakhov <brezerk@gmail.com>
2011-03-09 23:55:01 +0200

    Added Ron Widler details to Translator's tab;

--- src/q4wine-gui/about.cpp
+++ src/q4wine-gui/about.cpp
@@ -250,6 +250,12 @@ About::About(QWidget * parent, Qt::WFlags f) : QDialog(parent, f)
                             "</p><p style=\"margin: 0px\"> </p>" \
 
                             "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
+                                   "Ron Widler" \
+                            "</p><p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
+                                   "German translation" \
+                             "</p><p style=\"margin: 0px\"> </p>" \
+
+                            "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
                                    "Steven Panek (Espreon)" \
                             "</p><p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">" \
                                    "English fixes and translation" \
