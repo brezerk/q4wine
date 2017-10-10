45b8bf2b6262ac593e972030eb422e29e06b0a12
Oleksii S. Malakhov <brezerk@gmail.com>
2017-10-10 14:59:55 +0300

    Add app version to help url;

--- src/q4wine-lib/q4wine-lib.cpp
+++ src/q4wine-lib/q4wine-lib.cpp
@@ -1511,7 +1511,18 @@ QStringList corelib::getCdromDevices(void) const{
             url.append("/documentation/");
             url.append(lang);
             url.append("/");
-            url.append(rawurl);
+            if (rawurl.contains('#')) {
+                QStringList list = rawurl.split('#');
+		url.append(list.at(0));
+	        url.append("?version=");
+	        url.append(APP_VERS);
+		url.append('#');
+		url.append(list.at(1));
+            } else {
+                url.append(rawurl);
+	        url.append("?version=");
+	        url.append(APP_VERS);
+	    }
 
             this->openUrl(url);
             return;
