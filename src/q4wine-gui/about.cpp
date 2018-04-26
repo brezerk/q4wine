9c4072761457acbc5d3accfe41326b184f121ea2
Oleksii S. Malakhov <brezerk@gmail.com>
2018-04-27 00:19:21 +0300

    Update copyright: hello 2018 year :)

--- src/q4wine-gui/about.cpp
+++ src/q4wine-gui/about.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2008-2017 by Oleksii Malakhov                            *
+ *   Copyright (C) 2008-2018 by Oleksii Malakhov                            *
  *                                                                         *
  *   This program is free software: you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
